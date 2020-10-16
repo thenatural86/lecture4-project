af561f8fb92c4ac85b918811bc82a8ad355d787c
thenatural86 <philip.smalls86@gmail.com>
2020-10-16 12:10:05 -0400

    models and migrations

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+# makemigrations - Look for any changes that have been made to models.py
+# Then make a migration instruction for how to make those changes to the db
+
+# migrate - applies changes to the db
 # Create your models here.
 
 
@@ -7,18 +11,25 @@ class Airport(models.Model):
     code = models.CharField(max_length=3)
     city = models.CharField(max_length=64)
 
+    # returns a string representation of the object
     def __str__(self):
+        # formatted string
         return f"{self.city} ({self.code})"
 
 
 class Flight(models.Model):
+    # foreign key that references another table (the airport table)
+    # related_name - access relationship in the reverse order (Airport.departures)
     origin = models.ForeignKey(
         Airport, on_delete=models.CASCADE, related_name="departures")
     destination = models.ForeignKey(
         Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
+    # returns a string representation of the object
 
     def __str__(self):
+        # formatted string
+        # can access properties of object
         return f"{self.id}: {self.origin} to {self.destination}"
 
 
